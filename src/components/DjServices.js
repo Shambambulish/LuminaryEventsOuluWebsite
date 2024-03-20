5deb137405c8d25f1919455f784cfa2197b1515a
SamuelKoistinen <samuel.koistinen@gmail.com>
2024-03-20 14:04:55 +0200

    Dj page did fixing to the footer
    Updated Dj Page. Footer colors added correctly.

--- src/components/DjServices.js
+++ src/components/DjServices.js
@@ -2,26 +2,14 @@ const DjServices = () => {
 
     return (
         <div className="responsive-container">
-            <div className="slogan">Luminary Events Oulu, se täyden palvelun monipuolinen audiotalo.</div>
+            <div className="slogan">Luminary Events Oulu tarjoaa myös tapahtumia yritys- sekä yksityistilaisuuksiin Oulun ja Pohjois-Pohjanmaan alueella.</div>
             <br/>
-            <div className="textblock">Meiltä joustavat ratkaisut kaikenlaisiin tapahtumiin tarvitset sitten DJ:tä,
-                Bändin ääniteknikkoa tai Kaiutin- / valovuokrausta.
+            <div className="textblock">Meiltä löydät esimerkiksi esitystekniikan ratkaisut, ammattilaistason audioratkaisut ja DJ sekä ääni- / valomiespalvelut.
             </div>
             <br/>
-            <div className="subquestion">Miksi valita meidät?</div>
             <br/>
-            <div className="textblock">Jokaiseen tarjoukseen kuuluu kuljetus, asennus ja koko tapahtuma-ajan voimassa
-                oleva tekninen tuki, jolla voit olla varma tapahtumasi onnistumisesta.
-            </div>
-            <br/>
-            <div className="textblock">Lisäksi tarjolla myös edullista ja helppoa yksittäislaitevuokrausta.
-                Kymmenen vuoden kokemuksella, Luminary Events. Kysy ilmainen tarjous nyt!
-            </div>
-            <br/>
-            <div className="slogan">Kymmenen vuoden kokemuksella, Luminary Events.</div>
-            <div className="contactinfo">Sähköposti: contact@luminaryevents.fi
-
-                Puhelinnumero: +358 45 893 1112</div>
+            <div className="textblock">Järjestelmiä on eri kokoisiin tapahtumiin ja tarpeisiin. Saatavilla on myös DJ-laitteistoa
+        </div>
         </div>
     );
 };
