4e28b67ec760ae01136ebceec44cdd3e20c85ab2
Tino Roivanen <50320109+Tinosaurus@users.noreply.github.com>
2024-03-21 13:37:50 +0200

    Theming rest of the pages with placeholders

--- src/components/LargeEquipment.js
+++ src/components/LargeEquipment.js
@@ -1,36 +1,50 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import "./Frontpage.css";
+import logo from "./img/logo.png";
+import logo2 from "./img/Logo2.png"
 
 const LargeEquipment = () => {
-
+    //!!! banner grid not working quite as intended !!!
     return (
-        <div>
-        <Header/>
-        <div className="responsive-container">
-            <div className="slogan">Luminary Events Oulu, se täyden palvelun monipuolinen audiotalo.</div>
-            <br/>
-            <div className="textblock">Meiltä joustavat ratkaisut kaikenlaisiin tapahtumiin tarvitset sitten DJ:tä,
-                Bändin ääniteknikkoa tai Kaiutin- / valovuokrausta.
+        <div className="background">
+            <Header/>
+            <div className="banner">
+                <div className="textblock">Mainospuhe: mallia super epic</div>
             </div>
-            <br/>
-            <div className="subquestion">Miksi valita meidät?</div>
-            <br/>
-            <div className="textblock">Jokaiseen tarjoukseen kuuluu kuljetus, asennus ja koko tapahtuma-ajan voimassa
-                oleva tekninen tuki, jolla voit olla varma tapahtumasi onnistumisesta.
+            <div>
+                <div className="text-container">
+                    <div className="slogan">Luminary Events Oulu, se täyden palvelun monipuolinen audiotalo.</div>
+                    <br/>
+                    <div className="textblock">Meiltä joustavat ratkaisut kaikenlaisiin tapahtumiin tarvitset sitten
+                        DJ:tä,
+                        Bändin ääniteknikkoa tai Kaiutin- / valovuokrausta.
+                    </div>
+                    <br/>
+                    <div className="subquestion">Miksi valita meidät?</div>
+                    <br/>
+                    <div className="textblock">Jokaiseen tarjoukseen kuuluu kuljetus, asennus ja koko tapahtuma-ajan
+                        voimassa
+                        oleva tekninen tuki, jolla voit olla varma tapahtumasi onnistumisesta.
+                    </div>
+                    <br/>
+                    <div className="textblock">Lisäksi tarjolla myös edullista ja helppoa yksittäislaitevuokrausta.
+                        Kymmenen vuoden kokemuksella, Luminary Events. Kysy ilmainen tarjous nyt!
+                    </div>
+                    <br/>
+                    <div className="slogan">Kymmenen vuoden kokemuksella, Luminary Events.</div>
+                    <div className="contactinfo">
+                        <br/>
+                        Sähköposti: contact@luminaryevents.fi
+                        Puhelinnumero: +358 45 893 1112
+                        <br/>
+                    </div>
+                </div>
+                <div className="text-container">
+                    to be inserted: contact me frame
+                </div>
             </div>
-            <br/>
-            <div className="textblock">Lisäksi tarjolla myös edullista ja helppoa yksittäislaitevuokrausta.
-                Kymmenen vuoden kokemuksella, Luminary Events. Kysy ilmainen tarjous nyt!
-            </div>
-            <br/>
-            <div className="slogan">Kymmenen vuoden kokemuksella, Luminary Events.</div>
-            <div className="contactinfo">Sähköposti: contact@luminaryevents.fi
-
-                Puhelinnumero: +358 45 893 1112</div>
-
-                <p><h2>LARGE EQUIPMENT</h2></p>
-        </div>
-        <Footer/>
+            <Footer/>
         </div>
     );
 };
