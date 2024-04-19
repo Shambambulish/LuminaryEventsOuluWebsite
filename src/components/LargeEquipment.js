fd89dadbda33adf9f9e98598e05bef494def0041
SamuelKoistinen <samuel.koistinen@gmail.com>
2024-04-19 16:26:58 +0300

    Added Products to the lorem ipsum
    Pienille tapahtumille päivitetty sivu hintaluokkiin ja lisätty tuotteet. Hinnat pitää tarkistaa.

--- src/components/LargeEquipment.js
+++ src/components/LargeEquipment.js
@@ -12,20 +12,25 @@ const LargeEquipment = () => {
             <Header/>
             <br/>
             <div className="text-container">
-                <ConForm/>
             </div>
             <div className="dj-grid">
                 <div className="text-container-grid">
-                    <div className="slogan">Esimerkkipaketti pienille tapahtumille:</div>
+                    <div className="slogan">Esimerkkipaketti Pienille Tapahtumille</div>
                     <br/>
-                    <div className="textblock">Ipsum Lorem
+                    <div className="textblock">Pro112 Kokoäänet 2 kpl | 50€/vrk
+                    <br/>
+                    <br/>
+                    Proel115 Subwoofer 1 kpl | 15€/vrk
+                    <br/>
+                    <br/>
+                    Kaijuttimen tolpat | 75€/vrk 
+                    <br/>
+                    <br/>
+                    Asennus | 50€
                     </div>
                     <br/>
-                    <div className="subquestion">Paketin mukana sisältyvät laitteet</div>
+                    <div className="subquestion">Paketin mukana sisältyvät myös valoja tarvittaessa lisähintaan </div>
                     <br/>
-                    <div className="textblock">vastaus tähän
-
-                    </div>
                     <br/>
                     <div className="subquestion">Mitä lisälaitteita/kaapeleita/ym tämä paketti sisältää?
                     </div>
@@ -39,51 +44,42 @@ const LargeEquipment = () => {
             <br/>
             <div className="dj-grid">
                 <div className="text-container-grid">
-                    <div className="slogan">Esimerkkipaketti keskikokoisille tapahtumille:</div>
+                <div className="slogan">Esimerkkipaketti Keskikokoisille Tapahtumille</div>
                     <br/>
-                    <div className="textblock">Ipsum Lorem
+                    <div className="textblock">Pro112 Kokoäänet 2 kpl | 50€/vrk
+                    <br/>
+                    <br/>
+                    Passive18 Subwoofer 2 kpl | 50€/vrk
+                    <br/>
+                    <br/>
+                    Kaijuttimen tolpat | 120€/vrk 
+                    <br/>
+                    <br/>
+                    Behringer 6000D Vahvistin 1kpl | Lisää Hinta tähän
+                    <br/>
+                    <br/>
+                    Asennus | 50€
                     </div>
                     <br/>
-                    <div className="subquestion">Paketin mukana sisältyvät laitteet</div>
+                    <div className="subquestion">Paketin mukana sisältyvät myös valoja tarvittaessa lisähintaan </div>
                     <br/>
-                    <div className="textblock">vastaus tähän
-
-                    </div>
                     <br/>
                     <div className="subquestion">Mitä lisälaitteita/kaapeleita/ym tämä paketti sisältää?
                     </div>
                     <br/>
-                    <div className="textblock">vastaus tähän</div>
+                    <div className="textblock">Ei kuulu sinulle</div>
                 </div>
                 <div className="image-container">
                     <img src="https://placehold.co/500x500" alt="dj" className="LargeEquipmentImage"/>
                 </div>
             </div>
             <br/>
-            <div className="dj-grid">
-                <div className="text-container-grid">
-                    <div className="slogan">Esimerkkipaketti suurille tapahtumille:</div>
-                    <br/>
-                    <div className="textblock">Ipsum Lorem
-                    </div>
-                    <br/>
-                    <div className="subquestion">Paketin mukana sisältyvät laitteet</div>
-                    <br/>
-                    <div className="textblock">vastaus tähän
-
-                    </div>
-                    <br/>
-                    <div className="subquestion">Mitä lisälaitteita/kaapeleita/ym tämä paketti sisältää?
-                    </div>
-                    <br/>
-                    <div className="textblock">vastaus tähän</div>
-                </div>
-                <div className="image-container">
-                    <img src="https://placehold.co/500x500" alt="dj" className="LargeEquipmentImage"/>
-                </div>
-            </div>
             <div className="text-container">
+            <br/>
+            Jos ylläolevat paketit ei ole riittäviä tapahtumasi tarpeisiin ota yhteyttä alla olevalla lomakkeella!    
+            <ConForm/>
             </div>
+           
             <Footer/>
         </div>
     );
